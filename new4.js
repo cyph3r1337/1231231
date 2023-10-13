237e73e751d485af3ee603aa21f872647b50951c
cyph3r1337 <128782961+cyph3r1337@users.noreply.github.com>
2023-10-13 17:07:27 +0500

    Add files via upload

---
+++ new4.js
@@ -0,0 +1,8 @@
+let слово1 = "Привет";
+let слово2 = "Пока";
+
+if (слово1.charAt(0).toLowerCase() === слово2.charAt(0).toLowerCase()) {
+  console.log("Первые буквы совпадают");
+} else {
+  console.log("Первые буквы не совпадают");
+}
